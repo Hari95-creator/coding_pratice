ec67a2327aecd4e41c9abbbe7140bb63138c75b1
Harikrishnan <harisgkrishnan@yahoo.com>
2025-02-22 16:42:07 +0530

    coding pratice (21/02/2025)

---
+++ LongestSusbstringWithoutRepeating.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+
+public class LongestSusbstringWithoutRepeating {
+
+    //sliding window approach
+    // in xliding window it has two point sart( left ) and end (right )
+    
+    public static Integer findLongestSubstring(String word){
+
+        HashSet<Character> charSet=new HashSet<>();
+
+        int maxLength=0;
+        int left=0;
+
+        for(int right=0; right<word.length();right++){
+
+            char currentChar=word.charAt(right);
+
+            //this will remove  to shrink the window from left to right
+            while(charSet.contains(currentChar)){
+
+                charSet.remove(word.charAt(left));
+                left++;
+            }
+
+            charSet.add(currentChar);
+            maxLength=Math.max(maxLength,right-left+1);
+        }
+
+        return maxLength;
+
+
+    }
+
+    public static void main(String[] args){
+
+
+        String s="aaBBcd";
+
+        Integer count=findLongestSubstring(s);
+
+        System.out.println("Length of longest substring without repeating characters: " + count);
+
+    }
+    
+}
---
+++ StringCompression.java
@@ -0,0 +1,38 @@
+public class StringCompression {
+
+    public static String compressString(String s){
+
+        int count =0;
+        char currentChar=s.charAt(0);
+        StringBuilder compressed=new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i) == currentChar){
+
+                count++;
+                
+            }else{
+
+                compressed.append(currentChar).append(count);
+                currentChar=s.charAt(i);
+                count=1;
+
+            }
+
+
+        }
+
+
+        return compressed.toString();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        String input = "aabcccccaaa";
+        System.out.println("Compressed string: " + compressString(input));
+    }
+    
+}

919673e6201777c4696aa66cd505ca6dd0c03688
HARIKRISHNAN S G <harisgkrishnan@yahoo.com>
2025-08-22 15:29:01 +0530

    second highest with details

--- EmployeeSalary.java
+++ EmployeeSalary.java
@@ -1,75 +1,75 @@
 import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class EmployeeSalary{
+public class EmployeeSalary {
 
-    public static class Employee{
+    public static class Employee {
 
         private String empId;
         private String empName;
         private double empSalary;
 
+        public Employee(String empId, String empName, double empSalary) {
 
-        public Employee(String empId,String empName,double empSalary){
-
-            this.empId=empId;
-            this.empName=empName;
-            this.empSalary=empSalary;
+            this.empId = empId;
+            this.empName = empName;
+            this.empSalary = empSalary;
 
         }
 
-        public void setEmpId(String empId){
+        public void setEmpId(String empId) {
 
-            this.empId=empId;
+            this.empId = empId;
         }
 
-        public String getEmpId(){
+        public String getEmpId() {
 
             return empId;
         }
 
-        public void setEmpName(String empName){
+        public void setEmpName(String empName) {
 
-            this.empName=empName;
+            this.empName = empName;
         }
 
-        public String getEmpName(){
+        public String getEmpName() {
 
             return empName;
         }
 
-        public void setEmpSalary(double empSalary){
+        public void setEmpSalary(double empSalary) {
 
-            this.empSalary=empSalary;
+            this.empSalary = empSalary;
         }
 
-        public double getEmpSalary(){
+        public double getEmpSalary() {
 
             return empSalary;
         }
 
-
     }
 
-    public static void secondHighestSalary(List<Employee> employee){
+    public static void secondHighestSalary(List<Employee> employee) {
 
+        double firstHighestSalary = Double.MIN_VALUE;
+        Double secondHighestSalary = Double.MIN_VALUE;
 
+        Employee EMPLOYEEDETAILS = null;
 
-        double firstHighestSalary=Double.MIN_VALUE;
-        Double secondHighestSalary=Double.MIN_VALUE;
-        
-        Employee EMPLOYEEDETAILS=null;
+        for (Employee e : employee) {
 
-        for(Employee e : employee){
+            double sal = e.getEmpSalary();
 
-            double sal=e.getEmpSalary();
-
-            if(sal > firstHighestSalary){
+            if (sal > firstHighestSalary) {
 
                 secondHighestSalary = firstHighestSalary;
-                firstHighestSalary = sal;  
+                firstHighestSalary = sal;
 
-            }else if(sal > secondHighestSalary && sal < firstHighestSalary){
+            } else if (sal > secondHighestSalary && sal < firstHighestSalary) {
 
                 secondHighestSalary = sal;
 
@@ -83,43 +83,95 @@ public class EmployeeSalary{
 
         } else {
 
+            for (Employee details : employee) {
 
-            for(Employee details : employee ){
+                if (details.getEmpSalary() == secondHighestSalary) {
 
-
-                if(details.getEmpSalary()==secondHighestSalary){
-                    
-                    EMPLOYEEDETAILS=details;
+                    EMPLOYEEDETAILS = details;
                     break;
 
                 }
 
             }
 
-            System.out.println("Employee Name :"+EMPLOYEEDETAILS.getEmpName());
-            System.out.println("Employee Id :"+EMPLOYEEDETAILS.getEmpId());
+            System.out.println("**********************************************************************");
+            System.out.println("Employee Name :" + EMPLOYEEDETAILS.getEmpName());
+            System.out.println("Employee Id :" + EMPLOYEEDETAILS.getEmpId());
             System.out.println("Second Highest salary : " + secondHighestSalary);
-            
-            
+            System.out.println("**********************************************************************");
+
         }
-
-
 
     }
 
-    public static void main(String args[]){
+    public static void secondHighestSalryWithStreams(List<Employee> employees) {
 
-        
-        List<Employee> employeeData=Arrays.asList(
-            new Employee("1","Hari",75000),
-            new Employee("2","krishnan",50000),
-            new Employee("3","Sajin",85000),
-            new Employee("4","Sudha",5000)
-            );
+        double secondHighest = employees.stream()
+                .map(Employee::getEmpSalary)
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No salary found"));
 
+        System.out.println("**********************************************************************");
+
+        List<Employee> employeeDetails = employees.stream()
+                .filter(empdata -> empdata.getEmpSalary() == secondHighest)
+                .map(data -> new Employee(data.getEmpId(), data.getEmpName(), secondHighest))
+                .toList();
+
+        employeeDetails.forEach(eData -> {
+            System.out.println("Id :" + eData.getEmpId());
+            System.out.println("Name :" + eData.getEmpName());
+            System.out.println("Salary :" + eData.getEmpSalary());
+        });
+
+        System.out.println("**********************************************************************");
+
+        List<Employee> employeeData = employees.stream()
+                .filter(eObj -> eObj.getEmpSalary() == secondHighest)
+                .toList();
+
+        employeeData.forEach(eData -> {
+            System.out.println("Id :" + eData.getEmpId());
+            System.out.println("Name :" + eData.getEmpName());
+            System.out.println("Salary :" + eData.getEmpSalary());
+        });
+
+        System.out.println("**********************************************************************");
+
+        List<Employee> employeeD = employees.stream()
+                .filter(obj -> obj.getEmpSalary() == secondHighest)
+                .peek(eData ->
+
+                {
+                    System.out.println("Id using peek :" + eData.getEmpId());
+                    System.out.println("Name using peek:" + eData.getEmpName());
+                    System.out.println("Salary using peek:" + eData.getEmpSalary());
+                }).toList();
+        System.out.println("**********************************************************************");
+
+        // peek() is like forEach, but it’s an intermediate operation, so the stream can still be collected into a list afterward.
+
+        // forEach → consumes the stream, returns void.
+
+        // toList() (or collect(Collectors.toList())) → gives you a list.
+
+        // peek() → good if you want to log/print while still collecting.
+
+    }
+
+    public static void main(String args[]) {
+
+        List<Employee> employeeData = Arrays.asList(
+                new Employee("1", "Hari", 75000),
+                new Employee("2", "krishnan", 50000),
+                new Employee("3", "Sajin", 85000),
+                new Employee("4", "Sudha", 5000));
 
         secondHighestSalary(employeeData);
-
+        secondHighestSalryWithStreams(employeeData);
 
     }
 
